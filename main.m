2cc70f78347b0f84543c11f07b729d24ed6cf316
SwellPoem <piccione.2030930@studenti.uniroma1.it>
2024-08-02 17:55:18 +0200

    new collision

--- main.m
+++ main.m
@@ -19,11 +19,11 @@ q_ddot_sym = [q1_ddot; q2_ddot; q3_ddot];
 tau = sym('tau', [3, 1]);
 
 % lambda parameters for update
-lambda1 = diag([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]);
-lambda2 = diag([0.35, 0.35, 0.35, 0.35, 0.35, 0.35, 0.35, 0.35]);
+lambda1 = diag([0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9, 0.9]);
+lambda2 = diag([0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]);
 
 % redisual gain
-K = diag([0.5, 0.5, 0.5]);
+K = diag([0.2, 0.2, 0.2]);
 
 % controller gains
 Kp = diag([80, 80, 80]);
@@ -47,7 +47,7 @@ dynamic_eq = M * q_ddot_sym + C + G == tau;
 [Y, X] = regressor_matrix(M_par, C_par, G_par, m, I, l, dc, g0, q1_ddot, q2_ddot, q3_ddot);
 
 %% Excitation trajectory
-T = 20; % Total time
+T = 40; % Total time
 dt = 0.1; % Time step
 t = 0:dt:T; % Time vector
 
@@ -83,15 +83,19 @@ q_curr_history(:, 1) = q_curr;
 q_dot_curr_history(:, 1) = q_dot_curr;
 
 %% options
-options = odeset('RelTol',1e-1,'AbsTol',1e-1);
+options = odeset('RelTol',2e-1,'AbsTol',2e-1);
 
-up_threshold = 3.5;
-down_threshold = -5;
+up_threshold = 4;
+down_threshold = -3.5;
 
 %% external force parameters
-f_ext = [0; 0; 12];  % Force in Newtons
-f_ext_start = 6;  % Time when the force is applied
+f_ext = [-20; 0; 0];  % Force in Newtons
+f_ext_start = 4;  % Time when the force is applied
 f_ext_duration = 0.2;  % Duration of the force application
+
+f_ext2 = [0; -5; -25];  % Force in Newtons
+f_ext2_start = 27;  % Time when the force is applied
+f_ext2_duration = 0.25;  % Duration of the force application
 
 %% figure initialization
 figure;
@@ -103,131 +107,6 @@ ylim([-1 1]);
 zlim([0 1.1]);
 view(135, 5);
 arrow=quiver3(0,0,0,0,0,0,'AutoScaleFactor',1/40,'LineWidth',2,'LineStyle', '-', 'Color',[0 0 1], ShowArrowHead='on');
-
-% %% main loop
-% for i = 1:length(t)-1
-%     % update the joint angles
-%     Q_0(1).JointPosition = q_curr(1);
-%     Q_0(2).JointPosition = q_curr(2);
-%     Q_0(3).JointPosition = q_curr(3);
-% 
-%     % desired state
-%     q_des = q_traj(:, i);
-%     q_dot_des = q_dot_traj(:, i);
-%     q_ddot_des = q_ddot_traj(:, i);
-% 
-%     % inertia, Coriolis and gravity matrices of desired state
-%     M = double(subs(M, {q1, q2, q3, q1_dot, q2_dot, q3_dot}, ...
-%         {q_des(1), q_des(2), q_des(3), q_dot_des(1), q_dot_des(2), q_dot_des(3)}));
-%     C = double(subs(C, {q1, q2, q3, q1_dot, q2_dot, q3_dot}, ...
-%         {q_des(1), q_des(2), q_des(3), q_dot_des(1), q_dot_des(2), q_dot_des(3)}));
-%     G = double(subs(G, {q1, q2, q3}, {q_des(1), q_des(2), q_des(3)}));
-% 
-%     % estimated inertia, Coriolis, and gravity matrices
-%     M_hat = double(subs(M, {q1, q2, q3, q1_dot, q2_dot, q3_dot}, ...
-%         {q_curr(1), q_curr(2), q_curr(3), q_dot_curr(1), q_dot_curr(2), q_dot_curr(3)}));
-%     C_hat = double(subs(C, {q1, q2, q3, q1_dot, q2_dot, q3_dot}, ...
-%         {q_curr(1), q_curr(2), q_curr(3), q_dot_curr(1), q_dot_curr(2), q_dot_curr(3)}));
-%     G_hat = double(subs(G, {q1, q2, q3}, {q_curr(1), q_curr(2), q_curr(3)}));
-% 
-%     % compute control input tau
-%     tau = M_hat * (q_ddot_des + Kp * (q_des - q_curr) + Kd * (q_dot_des - q_dot_curr)) + C_hat .* q_dot_curr + G_hat;
-% 
-%     % dynamics integration to update the actual state
-%     [t_step, y_step] = ode45(@(t, y) dynamic_model(t, y, M_hat, C_hat, G_hat, tau), [t(i) t(i+1)], [q_curr; q_dot_curr], options);
-% 
-%     q_curr = y_step(end, 1:3).';
-%     q_dot_curr = y_step(end, 4:6).';
-% 
-% 
-%     % current acceleration
-%     q_ddot_curr = (q_dot_curr - prev_q_dot_curr) / dt;
-% 
-% 
-%      % external torque if the current time matches the external force time
-%     if t(i) >= f_ext_start && t(i) <= (f_ext_start + f_ext_duration)
-%         J_inv = get_J(q_curr(1), q_curr(2), q_curr(3))'; 
-%         tau_ext = J_inv * f_ext; 
-%     else
-%         tau_ext = [0; 0; 0];
-%     end
-% 
-%     % store actual values
-%     q_curr_history(:, i) = q_curr;
-%     q_dot_curr_history(:, i) = q_dot_curr;
-%     q_ddot_curr_history(:, i) = q_ddot_curr;
-% 
-%     tau_history(:, i) = tau;
-%     tau_ext_history(:, i) = tau_ext;
-% 
-%     % regressor matrix
-%     Y_curr = double(subs(Y, {q1, q2, q3, q1_dot, q2_dot, q3_dot, q1_ddot, q2_ddot, q3_ddot}, ...
-%         {q_curr(1), q_curr(2), q_curr(3), q_dot_curr(1), q_dot_curr(2), q_dot_curr(3), q_ddot_curr(1), q_ddot_curr(2), q_ddot_curr(3)}));
-%     % Y_curr = double(subs(Y, {q1, q2, q3, q1_dot, q2_dot, q3_dot, q1_ddot, q2_ddot, q3_ddot}, ...
-%     %     {q_traj(1), q_traj(2), q_traj(3), q_dot_traj(1), q_dot_traj(2), q_dot_traj(3), q_ddot_traj(1), q_ddot_traj(2), q_ddot_traj(3)}));
-% 
-%     % integrand
-%     integrand = tau - Y_curr * X_est - r_hat;
-% 
-%     % trapezoidal rule
-% 
-%     % if i > 1
-%     %     integral_term = integral_term + 0.5 * (integrand + prev_integrand) * dt;
-%     % else
-%     %     integral_term = integral_term + integrand * dt;
-%     % end
-%     integrand_history(:, i) = integrand;
-% 
-%     if i > 1
-%         integral_term = trapz(t(1:i), integrand_history(:, 1:i), 2);  % Compute the integral
-%     end
-% 
-%     % evolution of generalized momentum and its estimation
-%     p_dot = C'*q_dot_curr - G + tau + tau_ext;
-%     p_dot_hat = C_hat'*q_dot_curr - G_hat + tau + r_hat;
-% 
-%     % momentum residual
-%     r_hat = K * integral_term;
-% 
-%     % update momentum residual
-%     r_dot = K * (p_dot - p_dot_hat);
-% 
-%     % adaptive update of dynamic parameters
-%     X_dot_hat = lambda1^-1 * Y_curr.' * r_hat + lambda2^-1 * Y_curr.' * r_dot;
-%     X_est = X_est + X_dot_hat * dt;
-% 
-%     % Save values for plotting
-%     r_hat_history(:, i) = r_hat;
-%     X_est_history(:, i) = X_est;
-% 
-%     % save previous values for next iteration
-%     prev_integrand = integrand;
-%     prev_q_dot_curr = q_dot_curr;
-% 
-%     % Show
-%     show(robot, Q_0, 'PreservePlot', false, 'FastUpdate', 1, 'Collisions', 'on');
-%     % axis("equal");
-%     % xlim([-1 1]);
-%     % ylim([-1 1]);
-%     % zlim([0 1]);
-%     hold on;
-%     plot3(p_traj(1, :), p_traj(2, :), p_traj(3, :), 'r-', 'LineWidth', 1);
-% 
-%     % actual end-effector position
-%     end_effector_pos = get_end_eff_pos(q_curr(1), q_curr(2), q_curr(3));
-% 
-%     % plot the external force
-%     if t(i) >= f_ext_start && t(i) <= (f_ext_start + f_ext_duration)
-%         arrow.XData=end_effector_pos(1);
-%         arrow.YData=end_effector_pos(2);
-%         arrow.ZData=end_effector_pos(3);
-%         arrow.UData=f_ext(1);
-%         arrow.VData=f_ext(2);
-%         arrow.WData=f_ext(3);
-%     end
-% 
-%     drawnow;
-% end
 
 %% main loop
 for i = 1:length(t)-1
@@ -259,9 +138,17 @@ for i = 1:length(t)-1
     tau = M_hat * (q_ddot_des + Kp * (q_des - q_curr) + Kd * (q_dot_des - q_dot_curr)) + C_hat .* q_dot_curr + G_hat;
 
      % external torque if the current time matches the external force time
-    if t(i) >= f_ext_start && t(i) <= (f_ext_start + f_ext_duration)
-        J_inv = get_J(q_curr(1), q_curr(2), q_curr(3))'; 
-        tau_ext = J_inv * f_ext;
+    if (t(i) >= f_ext_start && t(i) <= (f_ext_start + f_ext_duration)) || (t(i) >= f_ext2_start && t(i) <= (f_ext2_start + f_ext2_duration))
+        
+        % Determine which external force to apply
+        if t(i) >= f_ext_start && t(i) <= (f_ext_start + f_ext_duration)
+            J_inv = get_J(q_curr(1), q_curr(2), q_curr(3))'; 
+            tau_ext = J_inv * f_ext;
+        elseif t(i) >= f_ext2_start && t(i) <= (f_ext2_start + f_ext2_duration)
+            J_inv = get_J(q_curr(1), q_curr(2), q_curr(3))'; 
+            tau_ext = J_inv * f_ext2;
+        end
+
         tau_new = tau + tau_ext;
 
         % dynamics integration to update the actual state
@@ -285,19 +172,12 @@ for i = 1:length(t)-1
         % regressor matrix
         Y_curr = double(subs(Y, {q1, q2, q3, q1_dot, q2_dot, q3_dot, q1_ddot, q2_ddot, q3_ddot}, ...
             {q_curr(1), q_curr(2), q_curr(3), q_dot_curr(1), q_dot_curr(2), q_dot_curr(3), q_ddot_curr(1), q_ddot_curr(2), q_ddot_curr(3)}));
-        % Y_curr = double(subs(Y, {q1, q2, q3, q1_dot, q2_dot, q3_dot, q1_ddot, q2_ddot, q3_ddot}, ...
-        %     {q_traj(1), q_traj(2), q_traj(3), q_dot_traj(1), q_dot_traj(2), q_dot_traj(3), q_ddot_traj(1), q_ddot_traj(2), q_ddot_traj(3)}));
-
+        
+        
         % integrand
         integrand = tau_new - Y_curr * X_est - r_hat;
     
         % trapezoidal rule
-
-        % if i > 1
-        %     integral_term = integral_term + 0.5 * (integrand + prev_integrand) * dt;
-        % else
-        %     integral_term = integral_term + integrand * dt;
-        % end
         integrand_history(:, i) = integrand;
 
         if i > 1
@@ -342,8 +222,15 @@ for i = 1:length(t)-1
             arrow.UData=f_ext(1);
             arrow.VData=f_ext(2);
             arrow.WData=f_ext(3);
+        elseif t(i) >= f_ext2_start && t(i) <= (f_ext2_start + f_ext2_duration)
+            arrow.XData=end_effector_pos(1);
+            arrow.YData=end_effector_pos(2);
+            arrow.ZData=end_effector_pos(3);
+            arrow.UData=f_ext2(1);
+            arrow.VData=f_ext2(2);
+            arrow.WData=f_ext2(3);
         end
- 
+
         drawnow;
     else
         tau_ext = [0; 0; 0];
@@ -369,19 +256,11 @@ for i = 1:length(t)-1
         % regressor matrix
         Y_curr = double(subs(Y, {q1, q2, q3, q1_dot, q2_dot, q3_dot, q1_ddot, q2_ddot, q3_ddot}, ...
             {q_curr(1), q_curr(2), q_curr(3), q_dot_curr(1), q_dot_curr(2), q_dot_curr(3), q_ddot_curr(1), q_ddot_curr(2), q_ddot_curr(3)}));
-        % Y_curr = double(subs(Y, {q1, q2, q3, q1_dot, q2_dot, q3_dot, q1_ddot, q2_ddot, q3_ddot}, ...
-        %     {q_traj(1), q_traj(2), q_traj(3), q_dot_traj(1), q_dot_traj(2), q_dot_traj(3), q_ddot_traj(1), q_ddot_traj(2), q_ddot_traj(3)}));
 
         % integrand
         integrand = tau - Y_curr * X_est - r_hat;
     
         % trapezoidal rule
-
-        % if i > 1
-        %     integral_term = integral_term + 0.5 * (integrand + prev_integrand) * dt;
-        % else
-        %     integral_term = integral_term + integrand * dt;
-        % end
         integrand_history(:, i) = integrand;
 
         if i > 1
@@ -432,6 +311,6 @@ end
 
 %% plots
 
-plot_results(t, r_hat_history, X_est_history, q_traj, q_curr_history, ...
+plot_results(t, r_hat_history, X_est_history, tau_history, tau_ext_history, q_traj, q_curr_history, ...
                       q_dot_traj, q_dot_curr_history, q_ddot_traj, q_ddot_curr_history, ...
-                      up_threshold, down_threshold, f_ext_start);
+                      up_threshold, down_threshold, f_ext_start, f_ext2_start);
--- plot_results.m
+++ plot_results.m
@@ -1,100 +1,142 @@
-function plot_results(t, r_hat_history, X_est_history, q_traj, q_curr_history, ...
+function plot_results(t, r_hat_history, X_est_history, tau_history, tau_ext_history, q_traj, q_curr_history, ...
                       q_dot_traj, q_dot_curr_history, q_ddot_traj, q_ddot_curr_history, ...
-                      up_threshold, down_threshold, f_ext_start)
+                      up_threshold, down_threshold, f_ext_start, f_ext2_start)
     figure;
     plot(t(1:end), r_hat_history);
-    title('Momentum Residual (kg*m/s)');
-    xlabel('Time (s)');
-    ylabel('Residual');
-    axis([min(t) max(t) -40 40]);
-    legend('r1', 'r2', 'r3');
-    yline(up_threshold, '--r');
-    yline(down_threshold, '--r');
-    xline(f_ext_start, '--b');
+    title("Momentum Residual","Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$kg * m/s$","Interpreter","Latex");
+    axis([min(t) max(t) -50 50]);
+    yline(up_threshold, "--r");
+    yline(down_threshold, "--r");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
+    legend("$r_{1}$","$r_{2}$","$r_{3}$", "up threshold", "down threshold", "collision time", "Interpreter","Latex");
+
+    % figure;
+    % plot(t(1:end-1), X_est_history);
+    % title("$ X_{est} $","Interpreter","Latex");
+    % xlabel("$ s $", "Interpreter","Latex");
+    % ylabel("$ X_{est} $","Interpreter","Latex");
+    % xline(f_ext_start, "--b");
+    % xline(f_ext2_start, "--b");
+    % grid on;
 
     figure;
-    plot(t(1:end-1), X_est_history);
-    title('X_est');
-    xlabel('Time (s)');
-    ylabel('X_est');
-    xline(f_ext_start, '--b');
+    plot(t(1:end), tau_ext_history);
+    title("external torques","Interpreter","Latex")
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ Nm $","Interpreter","Latex");
+    legend("$\tau_{ext,1}$","$\tau_{ext,2}$","$\tau_{ext,3}$", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
+
+    figure;
+    plot(t(1:end), tau_history);
+    title("joint torques","Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ Nm $","Interpreter","Latex");
+    legend("$\tau_{1}$","$\tau_{2}$","$\tau_{3}$", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     % Plot joint positions
     figure;
     subplot(3, 1, 1);
-    plot(t, q_traj(1, :), 'r', t, q_curr_history(1, :), 'b');
-    title('Joint 1 Position');
-    xlabel('Time (s)');
-    ylabel('Position (rad)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_traj(1, :), "r", t, q_curr_history(1, :), "b");
+    title("Joint 1 Position", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     subplot(3, 1, 2);
-    plot(t, q_traj(2, :), 'r', t, q_curr_history(2, :), 'b');
-    title('Joint 2 Position');
-    xlabel('Time (s)');
-    ylabel('Position (rad)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_traj(2, :), "r", t, q_curr_history(2, :), "b");
+    title("Joint 2 Position", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     subplot(3, 1, 3);
-    plot(t, q_traj(3, :), 'r', t, q_curr_history(3, :), 'b');
-    title('Joint 3 Position');
-    xlabel('Time (s)');
-    ylabel('Position (rad)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_traj(3, :), "r", t, q_curr_history(3, :), "b");
+    title("Joint 3 Position", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     % Plot joint velocities
     figure;
     subplot(3, 1, 1);
-    plot(t, q_dot_traj(1, :), 'r', t, q_dot_curr_history(1, :), 'b');
-    title('Joint 1 Velocity');
-    xlabel('Time (s)');
-    ylabel('Velocity (rad/s)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_dot_traj(1, :), "r", t, q_dot_curr_history(1, :), "b");
+    title("Joint 1 Velocity", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad/s $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     subplot(3, 1, 2);
-    plot(t, q_dot_traj(2, :), 'r', t, q_dot_curr_history(2, :), 'b');
-    title('Joint 2 Velocity');
-    xlabel('Time (s)');
-    ylabel('Velocity (rad/s)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_dot_traj(2, :), "r", t, q_dot_curr_history(2, :), "b");
+    title("Joint 2 Velocity", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad/s $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     subplot(3, 1, 3);
-    plot(t, q_dot_traj(3, :), 'r', t, q_dot_curr_history(3, :), 'b');
-    title('Joint 3 Velocity');
-    xlabel('Time (s)');
-    ylabel('Velocity (rad/s)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_dot_traj(3, :), "r", t, q_dot_curr_history(3, :), "b");
+    title("Joint 3 Velocity", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad/s $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     % Plot joint accelerations
     figure;
     subplot(3, 1, 1);
-    plot(t, q_ddot_traj(1, :), 'r', t, q_ddot_curr_history(1, :), 'b');
-    title('Joint 1 Acceleration');
-    xlabel('Time (s)');
-    ylabel('Acceleration (rad/s^2)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_ddot_traj(1, :), "r", t, q_ddot_curr_history(1, :), "b");
+    title("Joint 1 Acceleration", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad/s^2 $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     subplot(3, 1, 2);
-    plot(t, q_ddot_traj(2, :), 'r', t, q_ddot_curr_history(2, :), 'b');
-    title('Joint 2 Acceleration');
-    xlabel('Time (s)');
-    ylabel('Acceleration (rad/s^2)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_ddot_traj(2, :), "r", t, q_ddot_curr_history(2, :), "b");
+    title("Joint 2 Acceleration", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad/s^2 $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
     subplot(3, 1, 3);
-    plot(t, q_ddot_traj(3, :), 'r', t, q_ddot_curr_history(3, :), 'b');
-    title('Joint 3 Acceleration');
-    xlabel('Time (s)');
-    ylabel('Acceleration (rad/s^2)');
-    legend('Desired', 'Actual');
-    xline(f_ext_start, '--b');
+    plot(t, q_ddot_traj(3, :), "r", t, q_ddot_curr_history(3, :), "b");
+    title("Joint 3 Acceleration", "Interpreter","Latex");
+    xlabel("$ s $", "Interpreter","Latex");
+    ylabel("$ rad/s^2 $", "Interpreter","Latex");
+    legend("Desired", "Actual", "Interpreter","Latex");
+    xline(f_ext_start, "--b");
+    xline(f_ext2_start, "--b");
+    grid on;
 
 end
